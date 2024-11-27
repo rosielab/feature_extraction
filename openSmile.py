3f04278398fe51b58de071bc19d31c61050e7e72
Mantaj Dhillon <99550445+mantajdhillon@users.noreply.github.com>
2024-11-27 12:35:35 -0800

    Can get single Range and Mean for Features

--- openSmile.py
+++ openSmile.py
@@ -7,26 +7,78 @@
 # Step 1: pip install opensmile --user
 # Step 2: python openSmile.py
 
+import os
+import time
+
+import numpy as np
+import pandas as pd
+
+import audiofile
 import opensmile
 
+file = 'C:\\Users\\12364\\Desktop\\feature_extraction\\shout-data\\shout-data\\shout_data_00a0fb59-49f0-40e1-8481-6a334c65b126\\final\\chunk11.wav'
+signal, sampling_rate = audiofile.read(
+    file,
+    always_2d=True  # Keep this to ensure the output is always 2D
+)
 
 smile = opensmile.Smile(
     # feature_set=opensmile.FeatureSet.ComParE_2016,
-    feature_set=opensmile.FeatureSet.GeMAPSv01b,
-    feature_level=opensmile.FeatureLevel.Functionals,
+    feature_set=opensmile.FeatureSet.eGeMAPSv01a,
+    # feature_set=opensmile.FeatureSet.emobase,
+    feature_level=opensmile.FeatureLevel.LowLevelDescriptors,
+) 
+smile.feature_names
+['Loudness_sma3',
+ 'alphaRatio_sma3',
+ 'hammarbergIndex_sma3',
+ 'slope0-500_sma3',
+ 'slope500-1500_sma3',
+ 'spectralFlux_sma3',
+ 'mfcc1_sma3',
+ 'mfcc2_sma3',
+ 'mfcc3_sma3',
+ 'mfcc4_sma3',
+ 'F0semitoneFrom27.5Hz_sma3nz',
+ 'jitterLocal_sma3nz',
+ 'shimmerLocaldB_sma3nz',
+ 'HNRdBACF_sma3nz',
+ 'logRelF0-H1-H2_sma3nz',
+ 'logRelF0-H1-A3_sma3nz',
+ 'F1frequency_sma3nz',
+ 'F1bandwidth_sma3nz',
+ 'F1amplitudeLogRelF0_sma3nz',
+ 'F2frequency_sma3nz',
+ 'F2bandwidth_sma3nz',
+ 'F2amplitudeLogRelF0_sma3nz',
+ 'F3frequency_sma3nz',
+ 'F3bandwidth_sma3nz',
+ 'F3amplitudeLogRelF0_sma3nz']
+
+print("processing file now using GeMAPS\n")
+y = smile.process_signal(
+    signal,
+    sampling_rate
 )
-print("processing file now using GeMAPS")
-y = smile.process_file('C:\\Users\\12364\Desktop\\feature_extraction\\shout-data\\shout-data\\shout_data_00a0fb59-49f0-40e1-8481-6a334c65b126\\final\\chunk11.wav')
-print("done processing, will print now")
-# print(y)
+print("done processing, will print now\n")
+
+
+y.to_csv('C:\\Users\\12364\Desktop\\feature_extraction\\processed_features.csv', index=False)
 
 # printing the columns in the GeMAPS processing
 for col in y.columns:
     print(col)
 
-print("calculating all the means")
+print("\nCalculating all the ranges\n")
 
-means = y.mean(axis=0)  # Compute mean for all columns
+range_values = y.max(axis=0) - y.min(axis=0)
+print(range_values)
+
+############################################################
+print("\nCalculating all the means\n")
+
+# Compute range for all columns
+means = y.mean(axis=0)
 print(means)
 
 # print(smile.feature_set)
